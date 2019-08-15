e5fe73e1b21fbafd70f3607ecfde375abee92c56
retr0Tech <retr0tech@android-f645cc6c6332daab.localdomain>
2019-08-15 11:21:44 -0400

    Primera Entrega

---
+++ TestApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void LoginBtn_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(UserEntry.Text) || string.IsNullOrEmpty(PasswordEntry.Text))
+            {
+                await DisplayAlert("Error", "Debe llenar los campos para loguearse", "ok");
+            }
+            else
+            {
+                await DisplayAlert("Aviso", "Bienvenido a la plataforma", "ok");
+            }
+        }
+    }
+}
