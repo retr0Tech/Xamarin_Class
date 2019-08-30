73b449ab4752f0cb5bd13fd0a47d79eb150fd80c
retr0Tech <retr0tech@retr0Techs-MacBook-Air.local>
2019-08-30 18:40:43 -0400

    Second Page

--- TestApp/ViewModels/RegisterViewModel.cs
+++ TestApp/ViewModels/RegisterViewModel.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Windows.Input;
+using TestApp.Models;
+using Xamarin.Forms;
+using TestApp.Views;
+
 namespace TestApp.ViewModels
 {
     public class RegisterViewModel
     {
+        public User User { get; set; }
+        public ICommand clicked { get; set; }
         public RegisterViewModel()
         {
+            User = new User();
+
+            clicked = new Command(async () =>
+            {
+                if (string.IsNullOrEmpty(User._User) || string.IsNullOrEmpty(User.Password))
+                {
+                    await App.Current.MainPage.DisplayAlert("Error", "Debe llenar los campos para loguearse", "ok");
+                }
+                else
+                {
+                    await App.Current.MainPage.Navigation.PushAsync(new HomePage());
+                }
+            });
+
         }
     }
 }
